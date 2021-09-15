6b2c78c70dd96d21b3efc73be129e97bf5ca6fec
Patrick Wang <patwang123@berkeley.edu>
2021-09-14 19:51:53 -0700

    .

--- src/layout/cs61a/index.js
+++ src/layout/cs61a/index.js
@@ -3,7 +3,10 @@ import React from "react";
 const CS61A = () => {
   return (
     <div>
-      CS 61A page coming soon!
+      <div id="cs61a" className="section">
+        <h3 className="section-title">CS 61A</h3>
+        Not much here right now! If you want, check out my <a href="https://www.youtube.com/channel/UCmUBFBTt9TJunofdlvDw3oQ">youtube walkthrough videos</a>
+      </div>
     </div>
 
   )
